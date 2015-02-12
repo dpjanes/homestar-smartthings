60087504afc2a72bdb6992cb76d2a8c830a6b649
David Janes <davidjanes@davidjanes.com>
2015-02-12 09:06:51 -0500

    iotdb -> homestar + cleanup samples

--- samples/model_switch.js
+++ samples/model_switch.js
@@ -2,20 +2,21 @@
  *  Use a Model to manipulate semantically
  */
 
-var iotdb = require("iotdb");
+var homestar = require("homestar");
+var _ = homestar._;
 
 var ModelBinding = require('../SmartThingsSwitch');
 
-wrapper = iotdb.bridge_wrapper(ModelBinding.binding);
+wrapper = _.bridge_wrapper(ModelBinding.binding);
 wrapper.on('model', function(model) {
     model.on_change(function(model) {
         console.log("+ state\n ", model.thing_id(), model.state());
     });
     model.on_meta(function(model) {
-        console.log("+ meta\n ", model.thing_id(), model.meta().state());
+        console.log("+ meta\n ", model.thing_id(), _.ld.compact(model.meta().state()));
     });
     
-    console.log("+ discovered\n ", model.meta().state(), "\n ", model.thing_id());
+    console.log("+ discovered\n ", _.ld.compact(model.meta().state()), "\n ", model.thing_id());
 
         
     var on = false;
@@ -31,5 +32,5 @@ wrapper.on('model', function(model) {
     }, 2500);
 })
 wrapper.on('ignored', function(bridge) {
-    console.log("+ ignored\n ", bridge.meta());
+    console.log("+ ignored\n ", _.ld.compact(bridge.meta()));
 });
--- samples/model_temperature.js
+++ samples/model_temperature.js
@@ -2,22 +2,23 @@
  *  Use a Model to manipulate semantically
  */
 
-var iotdb = require("iotdb");
-var _compact = iotdb.helpers.ld.compact
+var homestar = require("homestar");
+var _ = homestar._;
+var _compact = homestar.helpers.ld.compact
 
 var ModelBinding = require('../SmartThingsTemperature');
 
-wrapper = iotdb.bridge_wrapper(ModelBinding.binding);
+wrapper = _.bridge_wrapper(ModelBinding.binding);
 wrapper.on('model', function(model) {
     model.on_change(function(model) {
         console.log("+ state\n ", model.thing_id(), model.state());
     });
     model.on_meta(function(model) {
-        console.log("+ meta\n ", model.thing_id(), _compact(model.meta().state()));
+        console.log("+ meta\n ", model.thing_id(), _.ld.compact(model.meta().state()));
     });
     
-    console.log("+ discovered\n ", _compact(model.meta().state()), "\n ", model.thing_id());
+    console.log("+ discovered\n ", _.ld.compact(model.meta().state()), "\n ", model.thing_id());
 })
 wrapper.on('ignored', function(bridge) {
-    console.log("+ ignored\n ", bridge.meta());
+    console.log("+ ignored\n ", _.ld.compact(bridge.meta()));
 });
