be00d4689a77973208a8e0c8c2293d74effb93b7
David Janes <davidjanes@davidjanes.com>
2015-02-25 18:15:31 -0500

    add IOTDB examples

---
+++ samples/iotdb_switch.js
@@ -0,0 +1,23 @@
+/*
+ */
+
+var iotdb = require("iotdb");
+var _ = iotdb._;
+
+var iot = iotdb.iot();
+var things = iot.connect("SmartThingsSwitch");
+things.on("thing", function(thing) {
+    console.log("+ discovered\n ", thing.thing_id());
+});
+things.on("state", function(thing) {
+    console.log("+ state\n ", thing.thing_id(), "\n ", thing.state());
+});
+things.on("meta", function(thing) {
+    console.log("+ meta\n ", thing.thing_id(), "\n ", _.ld.compact(thing.meta().state()));
+});
+    
+var on = false;
+var timer = setInterval(function() {
+    things.set("on", on);
+    on = !on;
+}, 2500);
