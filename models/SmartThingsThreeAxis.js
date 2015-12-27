895892bbd6e8b6b3112484caefdbc31e8a59aaec
David Janes <davidjanes@davidjanes.com>
2015-12-27 10:24:36 -0330

    missing IoTQL

--- models/SmartThingsThreeAxis.js
+++ models/SmartThingsThreeAxis.js
@@ -11,17 +11,8 @@
 
 "use strict";
 
-var iotdb = require("iotdb");
-
-exports.Model = iotdb.make_model('SmartThingsThreeAxis')
-    .facet(":sensor.spatial")
-    .i("x", iotdb.vector.number.xyz.x)
-    .i("y", iotdb.vector.number.xyz.y)
-    .i("z", iotdb.vector.number.xyz.z)
-    .make();
-
 exports.binding = {
-    model: exports.Model,
+    model: require('./SmartThingsThreeAxis.json'),
     bridge: require('../SmartThingsBridge').Bridge,
     initd: {
         device: 'threeAxis',
