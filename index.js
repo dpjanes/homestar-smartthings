6caa1e58255d67819ce1b678e7043915068a97b8
David Janes <davidjanes@davidjanes.com>
2016-08-29 06:13:23 -0400

    add bridge to homestar.json

--- index.js
+++ index.js
@@ -24,11 +24,11 @@
 
 exports.Bridge = require('./SmartThingsBridge').Bridge;
 exports.bindings = [
-    // require('./models/SmartThingsBattery').binding,
-    // require('./models/SmartThingsContact').binding,
-    // require('./models/SmartThingsMotion').binding,
-    // require('./models/SmartThingsPresence').binding,
+    require('./models/SmartThingsBattery').binding,
+    require('./models/SmartThingsContact').binding,
+    require('./models/SmartThingsMotion').binding,
+    require('./models/SmartThingsPresence').binding,
     require('./models/SmartThingsSwitch').binding,
-    // require('./models/SmartThingsTemperature').binding,
-    // require('./models/SmartThingsThreeAxis').binding,
+    require('./models/SmartThingsTemperature').binding,
+    require('./models/SmartThingsThreeAxis').binding,
 ];
